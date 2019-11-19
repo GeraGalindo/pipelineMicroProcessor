de55c52d55e6ac1c40b1bb24e620df292db0b96c
Galindo <GGalindo@luxoft.com>
2019-11-18 19:01:45 -0600

    Modify .do script, main module and testbench names from mips to pipeline

---
+++ pipeline.do
@@ -0,0 +1,42 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate -divider Instruction
+add wave -noupdate -label instruction /pipeline_tb/instruction
+add wave -noupdate -label clk /pipeline_tb/clk
+add wave -noupdate -label OPCODE /pipeline_tb_sv_unit::OP
+add wave -noupdate -label SA -radix decimal /pipeline_tb/SA
+add wave -noupdate -label RS -radix unsigned /pipeline_tb/RS
+add wave -noupdate -label RT -radix unsigned /pipeline_tb/RT
+add wave -noupdate -label RD -radix decimal /pipeline_tb/RD
+add wave -noupdate -label FUNC /pipeline_tb_sv_unit::FUNC
+add wave -noupdate -label IMMEDIATE -radix decimal /pipeline_tb/IMMEDIATE
+add wave -noupdate -divider {REGISTER FILE}
+add wave -noupdate -label RegFile -childformat {{{/pipeline_tb/dutPipeline/regFile/reg_file[0]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[1]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[2]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[3]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[4]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[5]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[6]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[7]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[8]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[9]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[10]} -radix decimal} {{/pipeline_tb/dutPipeline/regFile/reg_file[30]} -radix unsigned} {{/pipeline_tb/dutPipeline/regFile/reg_file[31]} -radix unsigned}} -subitemconfig {{/pipeline_tb/dutPipeline/regFile/reg_file[0]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[1]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[2]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[3]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[4]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[5]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[6]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[7]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[8]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[9]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[10]} {-height 15 -radix decimal} {/pipeline_tb/dutPipeline/regFile/reg_file[30]} {-height 15 -radix unsigned} {/pipeline_tb/dutPipeline/regFile/reg_file[31]} {-height 15 -radix unsigned}} /pipeline_tb/dutPipeline/regFile/reg_file
+add wave -noupdate -divider ALU
+add wave -noupdate -label A -radix unsigned /pipeline_tb/dutPipeline/iALU/A
+add wave -noupdate -label B -radix unsigned /pipeline_tb/dutPipeline/iALU/B
+add wave -noupdate -label OP -radix unsigned /pipeline_tb/dutPipeline/iALU/OP
+add wave -noupdate -label RESULT -radix unsigned /pipeline_tb/dutPipeline/iALU/RESULT
+add wave -noupdate -label Z /pipeline_tb/dutPipeline/iALU/Z
+add wave -noupdate -divider {DATA MEMORY}
+add wave -noupdate -label MEMORY /pipeline_tb/dataMem/memory
+add wave -noupdate -divider PC
+add wave -noupdate -label {Next Address} -radix decimal /pipeline_tb/dutPipeline/pc/Q
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {329000 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 209
+configure wave -justifyvalue left
+configure wave -signalnamewidth 0
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ps
+update
+WaveRestoreZoom {160200 ps} {344200 ps}
---
+++ pipeline_tb.sv
@@ -0,0 +1,331 @@
+// The smallest delay is #0.1 or 100ps
+`timescale 1ns/100ps;
+
+import func_pkg::*;
+
+func_t FUNC;
+opcode_t OP;
+
+// Testbench module name
+module pipeline_tb;
+
+// I/O ports from R-Type.sv are internal 
+// signals in the testbench.
+logic [31:0]instruction;
+logic clk;
+logic [31:0]NEXT_PC;
+
+logic [4:0]SA;
+logic [4:0]RS;
+logic [4:0]RT;
+logic [4:0]RD;
+
+logic [15:0]IMMEDIATE;
+logic [25:0]JmpAddr;
+
+logic DM_WE;
+logic [31:0]DM_D;
+logic [31:0]DM_Q;
+logic [15:0]DM_ADDR;
+
+pipeline dutPipeline(
+	.clk(clk),
+	.INSTRUCTION(instruction),
+	.DM_Q(DM_Q),
+	.DM_WE(DM_WE),
+	.DM_D(DM_D),
+	.DM_ADDR(DM_ADDR),
+	.NEXT_PC(NEXT_PC)
+	);
+	
+DATA_MEMORY dataMem(
+	.clk(clk),
+	.WE(DM_WE),
+	.D(DM_D),
+	.ADDR(DM_ADDR),
+	.Q(DM_Q)
+	);
+
+localparam real TCLK = 10;
+always begin
+    clk = 1'b0;
+    #(0.5*TCLK);
+    clk = 1'b1;
+    #(0.5*TCLK);
+end
+
+default clocking clock @(posedge clk);
+endclocking
+
+// Create some stimulus for the RType gate
+// initial blocks are executed only once
+
+/*
+	R-Type Instruction Encoding
+
+   |__OP__|__RS__|__RT__|__RD__|__SA__|__FUNC__|
+*/
+initial begin
+	//@@@@@@@@@@ R-TYPE INSTRUCTIONS START @@@@@@@@@@//
+    	// ADDITION
+	OP = RType; // R-Type
+	RS = 1; // Contains 1
+	RT = 2; // Contains 2
+	RD = 10; // Contains 10 but after Addition it should contain 3
+	SA = 0; // Unused field for a Addition
+	FUNC = ADD; // Addition
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// SUBSTRACTION
+	OP = RType; // R-Type
+	RS = 4; // Contains 4
+	RT = 2; // Contains 2
+	RD = 10; // Contains 3 but after Substraction it should contain 2
+	SA = 0; // Unused field for a Substraction
+	FUNC = SUB; // Substraction
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// Logical AND
+	OP = RType; // R-Type
+	RS = 5; // Contains 5
+	RT = 7; // Contains 7
+	RD = 10; // Contains 2 but after Logical AND it should contain 5
+	SA = 0; // Unused field for a Logical AND
+	FUNC = AND; // Logical AND
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// Logical OR
+	OP = RType; // R-Type
+	RS = 5; // Contains 5
+	RT = 2; // Contains 2
+	RD = 10; // Contains 5 but after Logical OR it should contain 7
+	SA = 0; // Unused field for a Logical OR
+	FUNC = OR; // Logical OR
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// Logical XOR
+	OP = RType; // R-Type
+	RS = 5; // Contains 5
+	RT = 3; // Contains 3
+	RD = 10; // Contains 7 but after Logical XOR it should contain 6
+	SA = 0; // Unused field for a Logical XOR
+	FUNC = XOR; // Logical XOR
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// Shift Left Logical
+	OP = RType; // R-Type
+	RS = 1; // Contains 1
+	RT = 2; // Contains 2
+	RD = 10; // Contains 6 but after Shift Left Logical it should contain 4
+	SA = 0; // Unused field for a Shift Left Logical
+	FUNC = SLL; // Shift Left Logical
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+
+	// Shift Right Logical
+	OP = RType; // R-Type
+	RS = 2; // Contains 6
+	RT = 4; // Contains 7
+	RD = 10; // Contains 4 but after Shift Right Logical it should contain 1
+	SA = 0; // Unused field for a Shift Right Logical
+	FUNC = SRL; // Shift Right Logical
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+	
+	// Shift Left Arithmetical
+	OP = RType; // R-Type
+	RS = 2; // Contains 6
+	RT = 4; // Contains 7
+	RD = 10; // Contains 4 but after Shift Right Logical it should contain 1
+	SA = 0; // Unused field for a Shift Right Logical
+	FUNC = SLA; // Shift Right Logical
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+	
+	// Shift Right Arithmetical
+	OP = RType; // R-Type
+	RS = 2; // Contains 6
+	RT = 16; // Contains 7
+	RD = 10; // Contains 4 but after Shift Right Logical it should contain 1
+	SA = 0; // Unused field for a Shift Right Logical
+	FUNC = SRA; // Shift Right Logical
+	instruction = {OP,RS,RT,RD,SA,FUNC};
+	##1
+	//@@@@@@@@@@ R-TYPE INSTRUCTIONS END @@@@@@@@@@//
+
+	//@@@@@@@@@@ I-TYPE INSTRUCTIONS START @@@@@@@@@@//
+	// ADDI
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// SUBI
+	OP = SUBI; //SUBI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// ANDI
+	OP = ANDI; //ANDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 3;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// ORI
+	OP = ORI; //ORI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 5;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// XORI
+	OP = XORI; //XORI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// LUI
+	OP = LUI; //LUI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 8;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// LLI
+	OP = LLI; //LLI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 8;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// SWR
+	OP = SWR; //SWR
+	RS = 2;
+	RT = 5;
+	IMMEDIATE = 2;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// LWR
+	OP = LWR; //LWR
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 10;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// SWI
+	OP = SWI; //SWI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 8;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// LWI
+	OP = LWI; //LWI
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 8;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 10;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// BEQ NOT BRANCHING
+	OP = BEQ;
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 0;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	
+##1
+	// BEQ BRANCHING
+	OP = BEQ;
+	RS = 2;
+	RT = 2;
+	IMMEDIATE = 0;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	// BNE NOT BRANCHING
+	OP = BNE;
+	RS = 2;
+	RT = 2;
+	IMMEDIATE = 0;
+	instruction = {OP,RS,RT,IMMEDIATE};
+
+##1
+	// BNE BRANCHING
+	OP = BNE;
+	RS = 2;
+	RT = 10;
+	IMMEDIATE = 0;
+	instruction = {OP,RS,RT,IMMEDIATE};
+##1
+	//@@@@@@@@@@ I-TYPE INSTRUCTIONS END @@@@@@@@@@//
+	
+	//@@@@@@@@@@ J-TYPE INSTRUCTIONS BEGIN @@@@@@@@@@//
+	OP = JMP;
+	JmpAddr = 42;
+	instruction = {OP,JmpAddr};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	OP = JAL;
+	JmpAddr = 24;
+	instruction = {OP,JmpAddr};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	OP = RET;
+	JmpAddr = 24;
+	instruction = {OP,JmpAddr};
+	##1
+	OP = ADDI; //ADDI
+	RS = 1;
+	RT = 10;
+	IMMEDIATE = 1;
+	instruction = {OP,RS,RT,IMMEDIATE};
+	##1
+	//@@@@@@@@@@ J-TYPE INSTRUCTIONS END @@@@@@@@@@//
+	$stop;
+end
+
+endmodule
+
